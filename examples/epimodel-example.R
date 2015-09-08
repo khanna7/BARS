df9cfd1834a839fe5f0712c2157535d2ccab4c80
Aditya Khanna <akhanna@MED32696.att.net>
2015-09-08 10:59:20 -0500

    EpiModel example complete

--- examples/epimodel-example.R
+++ examples/epimodel-example.R
@@ -2,3 +2,29 @@
 
 ## Packages and libraries
 library(EpiModel)
+
+## Packages and libraries
+library(network)# 3 Sep 2013
+library(ergm)#3 Sep 2013
+library(tergm)
+library(networkDynamic)
+
+   rm(list=ls())
+   ## Example 1
+   ## n=5000
+   ## 3000 edges
+   ## mean partnership duration = 10 timesteps
+   ## simulation over 50 timesteps
+   
+   nw <- network.initialize(5000, directed = FALSE)
+   formation <- ~edges
+   target.stats <- c(3000)
+   duration <- 10
+   
+   est_nw <- netest(nw, formation = formation, target.stats = target.stats,
+                    coef.diss = dissolution_coefs(~offset(edges), duration=duration))
+   est_nw
+   
+   dx <- netdx(est_nw, nsteps = 100, nsims = 1)
+   dx
+   
