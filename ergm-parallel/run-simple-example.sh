b951948506c6d2b012398ccefd02ddb725219e3e
Aditya Khanna <khanna7@uchicago.edu>
2016-01-29 11:51:43 -0600

    change ergm-parallel arguments to match the statnet example

--- ergm-parallel/run-simple-example.sh
+++ ergm-parallel/run-simple-example.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-#SBATCH --ntasks=10
+#SBATCH --nodes=2
 #SBATCH --time=60
 #SBATCH --mem-per-cpu=10000
+#SBATCH --exclusive
 
 # Always use -n 1 for the Rmpi package. It spawns additional processes dynamically
 mpirun -n 1 Rscript simple-ergm-fit-example.R
