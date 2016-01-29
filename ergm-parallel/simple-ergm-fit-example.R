c6fb0b9ed8ea1b5c863b67d8ee56cea585a18027
Aditya Khanna <khanna7@uchicago.edu>
2016-01-29 07:58:28 -0600

    improve example by adding dyadic dependent model that converges

--- ergm-parallel/simple-ergm-fit-example.R
+++ ergm-parallel/simple-ergm-fit-example.R
@@ -7,8 +7,8 @@
   library(methods)
 
   ## parameters
-  nresp <- 100
-  nalters <- 100
+  nresp <- 25
+  nalters <- 25
   mdeg <- 15
 
   n <- nresp + nalters
@@ -47,8 +47,13 @@
                      )
   print(proc.time()-t1)
 
-  ## Simulate once to generate a network
-  #
+  ## Fit different ERGM model 
+  respdeg <- sapply(1:n, function(x) sum(sim_net[x,1:nresp])) # Calculate degree with respondents
+  sim_net <- set.vertex.attribute(sim_net, 'respdeg', respdeg)  # Make nodal attribute
+
+  sim_fit_par<- ergm(sim_net ~ edges+nodecov('respdeg')+gwesp(1, fixed=TRUE),
+                               control=control.ergm(parallel=np, parallel.type="MPI")
+                     )
 
   ## Exit MPI                   
   stopCluster(cluster)
