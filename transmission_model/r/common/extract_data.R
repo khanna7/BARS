a846361426226eb9ecb5c7caf750187cd5af03ac
Aditya Khanna <khanna7@uchicago.edu>
2016-03-23 15:51:22 -0500

    add partnership level extraction

--- transmission_model/r/common/extract_data.R
+++ transmission_model/r/common/extract_data.R
@@ -41,6 +41,28 @@
       summary(last.viral.load) # everyone
       summary(last.viral.load[ug.inf]) # infected only
 
+      ## list discordant partnerships
+      ug.nw.el <- as.edgelist(network.extract(ug.nw,
+                                              at = time,
+                                              retain.all.vertices = T)) 
+
+      status.el <- matrix((ug.nw %v% "inf.status")[ug.nw.el], ncol = 2)
+      inf.time <- ug.nw %v% "inf.time"
+      time.since.infection <- ug.nw %v% "time.since.infection" # ASK
+
+
+      discordant.mpos <- intersect(which(status.el[, 1] == 1),
+                                    which(status.el[, 2] == 0)) #male positive, female negative
+
+      transmittable.m <- ug.nw.el[discordant.mpos, 1]
+      infectible.f <- ug.nw.el[discordant.mpos, 2]
+
+      discordant.fpos <- intersect(which(status.el[, 1] == 0),
+                                    which(status.el[, 2] == 1)) #female positive, male negative
+
+      infectible.m <- ug.nw.el[discordant.fpos, 1]
+      transmittable.f <- ug.nw.el[discordant.fpos, 2]
+
       ## incidence: can be computed from generated csv, by computing the number of new infections/number uninfected at every time step
 
       ## demographic parameters: i recorded number of births, deaths and associated parameters in a separate csv that was generated at each time step
