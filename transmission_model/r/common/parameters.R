7b6794f6544aa1c4f4676acb3fd26e843dfd2c14
Aditya Khanna <khanna7@uchicago.edu>
2016-04-03 22:25:54 -0500

    lower entry rate to 0.0001

--- transmission_model/r/common/parameters.R
+++ transmission_model/r/common/parameters.R
@@ -17,7 +17,7 @@
    min.age <- 16
    max.age <- 65
    given.dur.inf.by.age <- 10*365/size.of.timestep #(not age-specific yet)  
-   daily.entry.rate <- 0.001
+   daily.entry.rate <- 0.0001
    ## distribution of ages (between min and max)
    ## number of births (n.births: for now take it as 1% per year)
    ## age-specific mortality rates (ASMR), adjusted for HIV/AIDS-related deaths
