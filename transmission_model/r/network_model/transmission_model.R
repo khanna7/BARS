2038b478690b085a1a0821f5cd0c47da3222a9a5
Aditya Khanna <khanna7@uchicago.edu>
2016-05-16 10:39:07 -0500

    increase control parameters

--- transmission_model/r/network_model/transmission_model.R
+++ transmission_model/r/network_model/transmission_model.R
@@ -60,7 +60,8 @@ nw_simulate <-
 
                    time.start = time,
 
-                   control = control.simulate.network(),
+                   control = control.simulate.network(MCMC.burnin.min=1e5,
+                       MCMC.burnin.max=1e7),
 
                    time.slices = 1
 
