7b109e7cc348337ea61e3f357c17b27895dd7739
jozik <xio247@gmail.com>
2016-07-18 13:34:40 -0500

    Update midway_test_sweep.sh

--- transmission_model/swift_proj/swift/midway_test_sweep.sh
+++ transmission_model/swift_proj/swift/midway_test_sweep.sh
@@ -4,6 +4,7 @@ export WALLTIME=00:10:00
 export PROCS=8
 export PPN=8
 
-export T_PROJECT_ROOT=$PWD/..
+THIS=$( dirname $0 )
+export T_PROJECT_ROOT=$( cd $THIS/.. ; /bin/pwd )
 
 swift-t -m slurm -p test_sweep.swift -f=$T_PROJECT_ROOT/data/test_input_params.txt -md=$PWD/../../Release -props=$PWD/../../config/model.props
