fe0c3b78a701ba1e23c29cc80aeb9de0ebaf896f
jozik <xio247@gmail.com>
2016-08-01 12:11:23 -0500

    Added experiment label.

--- transmission_model/swift_proj/swift/midway_test_sweep.sh
+++ transmission_model/swift_proj/swift/midway_test_sweep.sh
@@ -3,9 +3,10 @@ export QUEUE=sandyb
 export WALLTIME=00:10:00
 export PROCS=8
 export PPN=8
-export TURBINE_JOBNAME="tm_XX"
+export EXP_NAME="expID"
+export TURBINE_JOBNAME="tm_"$EXP_NAME
 
 THIS=$( dirname $0 )
 export T_PROJECT_ROOT=$( cd $THIS/.. ; /bin/pwd )
 
-swift-t -m slurm -p test_sweep.swift -f=$T_PROJECT_ROOT/data/test_input_params.txt -md=$PWD/../../Release -props=$PWD/../../config/model.props
+swift-t -m slurm -p test_sweep.swift -exp=$EXP_NAME -f=$T_PROJECT_ROOT/data/test_input_params.txt -md=$PWD/../../Release -props=$PWD/../../config/model.props
