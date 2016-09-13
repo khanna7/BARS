bf3e0d4a7dd2742863f9ed3d7098d7eefc531d51
Aditya Khanna <khanna7@uchicago.edu>
2016-09-13 08:55:22 -0500

    update executable name in directory

--- transmission_model/swift_proj/scripts/trans_model.sh
+++ transmission_model/swift_proj/scripts/trans_model.sh
@@ -10,6 +10,6 @@ echo "cding to $model_dir"
 cd $model_dir
 
 #app_name=transmission-model-0.0
-app_name=transmission_model-testing_diagnosis-0.2
+app_name=transmission_model-testing_diagnosis-0.3
 
 mpirun -n 1 ./$app_name $props_file $param_line
