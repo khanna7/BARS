5de0208f778ede1fa003a319620cc8c9c38af850
Aditya Khanna <khanna7@uchicago.edu>
2017-01-16 12:23:31 -0600

    vno to 0.7

--- transmission_model/swift_proj/scripts/trans_model.sh
+++ transmission_model/swift_proj/scripts/trans_model.sh
@@ -39,7 +39,7 @@ instance_directory=$3
 cd $instance_directory
 
 # TODO: Define the command to run the model
-APP=$emews_root/../Release/transmission_model-0.6
+APP=$emews_root/../Release/transmission_model-0.7
 PROPS_FILE=$emews_root/../config/model.props
 MODEL_CMD="mpirun -n 1 $APP $PROPS_FILE $param_line"
 
