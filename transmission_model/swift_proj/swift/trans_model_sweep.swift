c094a7c1f8fb09b5f8bae8ccbf2fc9ee964a4bcf
Nick Collier <ncollier@anl.gov>
2017-03-07 16:11:07 -0500

    Added post processing step

--- transmission_model/swift_proj/swift/trans_model_sweep.swift
+++ transmission_model/swift_proj/swift/trans_model_sweep.swift
@@ -1,9 +1,21 @@
 import io;
 import sys;
 import files;
+import R;
+import string;
 
 string emews_root = getenv("EMEWS_PROJECT_ROOT");
 string turbine_output = getenv("TURBINE_OUTPUT");
+
+string summarize_template = """
+source('%s/R/summarize_functions.R')
+f <- %s
+prev <- summarize_prev(f)
+inc <- summarize_inc(f)
+pop_size <- summarize_pop_size(f)
+a <- paste(paste(prev, collapse=","), paste(inc, collapse=","),
+  paste(pop_size, collapse=","), sep = ",")
+""";
 
 app (file out, file err) run_model (file shfile, string param_line, string instance)
 {
@@ -26,12 +38,21 @@ run_prerequisites() => {
   file model_sh = input(emews_root+"/scripts/trans_model.sh");
   file upf = input(argv("f"));
   string upf_lines[] = file_lines(upf);
+  string results[];
   foreach s,i in upf_lines {
     string instance = "%s/instance_%i/" % (turbine_output, i+1);
     make_dir(instance) => {
       file out <instance+"out.txt">;
       file err <instance+"err.txt">;
-      (out,err) = run_model(model_sh, s, instance);
+      (out,err) = run_model(model_sh, s, instance); //=> {
+        //string summarize_code = summarize_template % (emews_root, instance + "output/counts.csv");
+        // string result = R(summarize_code, "a");
+        //results[i] = "%i,%s" % (i + 1, result);
+      //}
     }
   }
+
+  string fname = "%s/final_results.csv" % turbine_output;
+  file results_file <fname> = write(join(results, "\n"));
+  printf("Writing final result to %s", fname);
 }
