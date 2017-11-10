7fc94ea5cd889c896ec496fea8366808c8f28f51
Aditya Khanna <khanna7@uchicago.edu>
2017-11-10 13:11:46 -0600

    fix summarize function

--- transmission_model/swift_proj/R/summarize_functions.R
+++ transmission_model/swift_proj/R/summarize_functions.R
@@ -42,6 +42,29 @@ inc_func <- function(counts, counts_yr_chunks, numerators, denominator) {
   mean_inc_ten_yrs <- tail(mean_inc, 10)
   mean_inc_ten_yrs <- unlist(mean_inc_ten_yrs)*365*100 
   result <- round(mean_inc_ten_yrs, 3)
+  
+}
+
+calc_new_inf <- function(counts, counts_yr_chunks, numerators) {
+  #print(length(numerators))
+  
+  if (length(numerators) == 1) {
+    sum_new_inf <- lapply(counts_yr_chunks, function (x)
+      sum(x[2:nrow(x), numerators])
+    )
+  } else if (length(numerators) == 2) {
+    sum_new_inf <- lapply(counts_yr_chunks, function (x)
+      sum((x[2:nrow(x), numerators[1]] + x[2:nrow(x), numerators[2]]))
+    )
+  } else if (length(numerators) == 3) {
+    sum_new_inf <- lapply(counts_yr_chunks, function (x)
+      sum((x[2:nrow(x), numerators[1]] + x[2:nrow(x), numerators[2]] + x[2:nrow(x), numerators[3]]))
+    )
+  }
+
+
+  mean_new_inf <- mean(unlist(sum_new_inf))
+  result <- round(mean_new_inf, 3)
   return(result)
 }
 
@@ -68,12 +91,30 @@ summarize_inc <- function(filename="counts.csv"){
   ee_B_result <- inc_func(counts, counts_yr_chunks, c("infected_via_transmission_B", "infected_external_B", 
                                                           "infected_at_entry_B"), "uninfected_B")
 
-   return(c(full_result, a_result, b_result, ex_full_result, ex_A_result, ex_B_result,
-            ee_full_result, ee_A_result, ee_B_result))
+
+  newinf_full_result <- calc_new_inf(counts, counts_yr_chunks, c("infected_via_transmission"))
+  newinf_a_result <- calc_new_inf(counts, counts_yr_chunks, c("infected_via_transmission_A"))
+  newinf_b_result <- calc_new_inf(counts, counts_yr_chunks, c("infected_via_transmission_B"))
+
+  newinf_ex_full_result <- calc_new_inf(counts, counts_yr_chunks, c("infected_via_transmission", "infected_externally"))
+  newinf_ex_a_result <- calc_new_inf(counts, counts_yr_chunks, c("infected_via_transmission_A", "infected_external_A"))
+  newinf_ex_b_result <- calc_new_inf(counts, counts_yr_chunks, c("infected_via_transmission_B", "infected_external_B"))
+
+  newinf_ee_full_result <- calc_new_inf(counts, counts_yr_chunks, c("infected_via_transmission", "infected_externally", "infected_at_entry"))
+  newinf_ee_a_result <- calc_new_inf(counts, counts_yr_chunks, c("infected_via_transmission_A", "infected_external_A", "infected_at_entry_A"))
+  newinf_ee_b_result <- calc_new_inf(counts, counts_yr_chunks, c("infected_via_transmission_B", "infected_external_B", "infected_at_entry_B"))
+  
+  
+   return(c(full_result, a_result, b_result,
+            ex_full_result, ex_A_result, ex_B_result,
+            ee_full_result, ee_A_result, ee_B_result,
+            newinf_full_result, newinf_a_result, newinf_b_result,
+            newinf_ex_full_result, newinf_ex_a_result, newinf_ex_b_result,
+            newinf_ee_full_result, newinf_ee_a_result, newinf_ee_b_result
+            ))
 }
 
 summarize_pop_size <- function(filename="counts.csv"){
-  
-  counts <- read.csv(filename)
-  return(c(tail(counts$vertex_count, 1), tail(counts$vertex_count_A, 1), tail(counts$vertex_count_B, 1)))
+    counts <- read.csv(filename)
+    return(c(tail(counts$vertex_count, 1), tail(counts$vertex_count_A, 1), tail(counts$vertex_count_B, 1)))
 }
