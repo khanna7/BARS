a28e90342f0b8e9a664c0f196f7ed249a6a8eb88
Nick Collier <ncollier@anl.gov>
2018-01-04 09:15:09 -0500

    Script file as parameter

--- transmission_model/swift_proj/swift/run_trans_model_sweep.sh
+++ transmission_model/swift_proj/swift/run_trans_model_sweep.sh
@@ -45,6 +45,8 @@ export TURBINE_JOBNAME="${EXPID}_job"
 # command line arguments to the swift script.
 CMD_LINE_ARGS="$*"
 
+MODEL_SH=$EMEWS_ROOT/scripts/trans_model.sh
+
 # set machine to your schedule type (e.g. pbs, slurm, cobalt etc.),
 # or empty for an immediate non-queued unscheduled run
 MACHINE="slurm"
@@ -66,4 +68,7 @@ set -x
 
 #swift-t -n $PROCS $MACHINE -p $EMEWS_PROJECT_ROOT/swift/trans_model_sweep.swift -f="$EMEWS_PROJECT_ROOT/data/small_runs.txt" $CMD_LINE_ARGS
 
-swift-t -n $PROCS $MACHINE -p $EMEWS_PROJECT_ROOT/swift/trans_model_sweep.swift -f="$EMEWS_PROJECT_ROOT/data/upf_Morris_1_seeds1to10.txt" $CMD_LINE_ARGS
+swift-t -n $PROCS $MACHINE -p $EMEWS_PROJECT_ROOT/swift/trans_model_sweep.swift \
+    -f="$EMEWS_PROJECT_ROOT/data/upf_Morris_1_seeds1to10.txt" \
+    -model_sh=$MODEL_SH \
+    $CMD_LINE_ARGS
