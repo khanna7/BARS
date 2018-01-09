ef6e7b55f483042b30cf60f27c98643a60bcd81c
Nick Collier <ncollier@anl.gov>
2018-01-09 11:13:07 -0500

    First cut at crashed run handling

--- transmission_model/swift_proj/swift/trans_model_sweep.swift
+++ transmission_model/swift_proj/swift/trans_model_sweep.swift
@@ -12,12 +12,26 @@ string model_sh_file = argv("model_sh");
 string summarize_template = """
 source('%s/R/summarize_functions.R')
 f <- '%s'
-prev <- summarize_prev(f)
-inc <- summarize_inc(f)
-pop_size <- summarize_pop_size(f)
-a <- paste(paste(prev, collapse=','), paste(inc, collapse=','),
-  paste(pop_size, collapse=','), sep = ',')
+a <- ''
+if (file.exists(f)) {
+  prev <- summarize_prev(f)
+  inc <- summarize_inc(f)
+  pop_size <- summarize_pop_size(f)
+  a <- paste(paste(prev, collapse=','), paste(inc, collapse=','),
+    paste(pop_size, collapse=','), sep = ',')
+}
 """;
+
+string run_number_template = """
+run_number = %i
+param_line = '%s'
+if param_line.find('run.number') != -1:
+    params = param_line.split(',')
+    v = next(i for i in params if i.split('=')[0] == 'run.number')
+    run_number = int(v.split('=')[1])
+""";
+
+
 
 app (file out, file err) run_model (file shfile, string param_line, string instance)
 {
@@ -43,19 +57,27 @@ run_prerequisites() => {
   string upf_lines[] = file_lines(upf);
   string results[];
   foreach s,i in upf_lines {
-    string instance = "%s/instance_%i/" % (turbine_output, i+1);
+    string py_code = run_number_template % (i, s);
+    string run_num = python(py_code, "str(run_number)");
+    string instance = "%s/instance_%i/" % (turbine_output, run_num);
     make_dir(instance) => {
       file out <instance+"out.txt">;
       file err <instance+"err.txt">;
       (out,err) = run_model(model_sh, s, instance) => {
         string summarize_code = summarize_template % (emews_root, instance + "output/counts.csv");
         string result = R(summarize_code, "a");
-        results[i] = "%i,%s" % (i + 1, result);
+        if (length(result) > 0) {
+          results[i] = "%i,%s" % (i + 1, result);
+        } else {
+          results[i] = "";
+        }
       }
     }
   }
 
+  string finals[] = pick_regexp(".+", results);
+
   string fname = "%s/final_results.csv" % turbine_output;
-  file results_file <fname> = write(join(results, "\n") + "\n") =>
+  file results_file <fname> = write(join(finals, "\n") + "\n") =>
   printf("Writing final result to %s", fname);
 }
