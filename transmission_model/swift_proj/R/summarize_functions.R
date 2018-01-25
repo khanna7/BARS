f22e9955bab5b12113e90501d8c429810617e5b4
Nick Collier <ncollier@anl.gov>
2018-01-25 13:47:40 -0500

    Updated summarize functions

--- transmission_model/swift_proj/R/summarize_functions.R
+++ transmission_model/swift_proj/R/summarize_functions.R
@@ -1,43 +1,54 @@
 ## summarize functions
 
-summarize_prev <- function(filename="counts.csv"){
+summarize_prev <- function(filename="counts.csv", ranges){
   
   counts <- read.csv(filename)
   counts <- counts[-1,]
+  
   prev <- (counts$vertex_count - counts$uninfected)/(counts$vertex_count)
   prev <- prev*100
   prev_last_ten_yrs <- tail(prev, 3650)
   mean_sd <- c(mean(prev_last_ten_yrs), sd(prev_last_ten_yrs))
   
-  prev <- (counts$vertex_count_A - counts$uninfected_A)/(counts$vertex_count_A)
-  prev <- prev*100
-  prev_last_ten_yrs <- tail(prev, 3650)
-  mean_sd_A <- c(mean(prev_last_ten_yrs), sd(prev_last_ten_yrs))
+  means <- sapply(seq(1, length(ranges) - 1, by=2), function(x) {
+    v_counts <- sprintf("vertex_count_%s", seq(ranges[x], ranges[x+1]))
+    sum_v_counts <- rowSums(counts[v_counts])
+   
+    uninfs <- sprintf("uninfected_%s", seq(ranges[x], ranges[x+1]))
+    sum_unifs <- rowSums(counts[uninfs])
+    
+    prev <- (sum_v_counts - sum_unifs) / (sum_v_counts)
+    prev <- prev*100
+    prev_last_ten_yrs <- tail(prev, 3650)
+    mean_sd_A <- c(mean(prev_last_ten_yrs), sd(prev_last_ten_yrs))
+    
+  })
   
-  prev <- (counts$vertex_count_B - counts$uninfected_B)/(counts$vertex_count_B)
-  prev <- prev*100
-  prev_last_ten_yrs <- tail(prev, 3650)
-  mean_sd_B <- c(mean(prev_last_ten_yrs), sd(prev_last_ten_yrs))
-  
-  return(c(mean_sd, mean_sd_A, mean_sd_B))
+  return(unlist(c(mean_sd, means)))
 }
 
-inc_func <- function(counts, counts_yr_chunks, numerators, denominator) {
-  #print(length(numerators))
+calc_denom <- function(x, denominator) {
+  if (length(denominator) == 1) {
+    denom <- x[1:(nrow(x) - 1), denominator]
+  } else {
+    denom <- Reduce("+", (x[1:(nrow(x) - 1), denominator]))
+  }
+}
+
+inc_func <- function(counts_yr_chunks, numerators, denominator) {
   
   if (length(numerators) == 1) {
-    mean_inc <- lapply(counts_yr_chunks, function (x)
-      mean(x[2:nrow(x), numerators] / x[1:(nrow(x) - 1), denominator])
-    )
-  } else if (length(numerators) == 2) {
-    mean_inc <- lapply(counts_yr_chunks, function (x)
-      mean((x[2:nrow(x), numerators[1]] + x[2:nrow(x), numerators[2]]) / x[1:(nrow(x) - 1), denominator])
-    )
-  } else if (length(numerators) == 3) {
-    mean_inc <- lapply(counts_yr_chunks, function (x)
-      mean((x[2:nrow(x), numerators[1]] + x[2:nrow(x), numerators[2]] + x[2:nrow(x), numerators[3]]) / x[1:(nrow(x) - 1), denominator])
-    )
-  }
+    mean_inc <- lapply(counts_yr_chunks, function (x) {
+      denom <- calc_denom(x, denominator)
+      mean(x[2:nrow(x), numerators] / denom)
+    })
+  } else  {
+    mean_inc <- lapply(counts_yr_chunks, function (x) {
+      denom <- calc_denom(x, denominator)
+      z <- Reduce("+", (x[2:nrow(x), numerators]))
+      mean(z / denom)
+    })
+  } 
   
   mean_inc_ten_yrs <- tail(mean_inc, 10)
   mean_inc_ten_yrs <- unlist(mean_inc_ten_yrs)*365*100 
@@ -45,76 +56,92 @@ inc_func <- function(counts, counts_yr_chunks, numerators, denominator) {
   
 }
 
-calc_new_inf <- function(counts, counts_yr_chunks, numerators) {
+calc_new_inf <- function(counts_yr_chunks, numerators) {
   #print(length(numerators))
   
   if (length(numerators) == 1) {
     sum_new_inf <- lapply(counts_yr_chunks, function (x)
       sum(x[2:nrow(x), numerators])
     )
-  } else if (length(numerators) == 2) {
+  } else {
     sum_new_inf <- lapply(counts_yr_chunks, function (x)
-      sum((x[2:nrow(x), numerators[1]] + x[2:nrow(x), numerators[2]]))
-    )
-  } else if (length(numerators) == 3) {
-    sum_new_inf <- lapply(counts_yr_chunks, function (x)
-      sum((x[2:nrow(x), numerators[1]] + x[2:nrow(x), numerators[2]] + x[2:nrow(x), numerators[3]]))
+      sum(Reduce("+", (x[2:nrow(x), numerators])))
     )
   }
-
-
+    
   mean_new_inf <- mean(unlist(sum_new_inf))
   result <- round(mean_new_inf, 3)
   return(result)
 }
 
-summarize_inc <- function(filename="counts.csv"){
+summarize_inc <- function(filename="counts.csv", ranges){
   
   counts <- read.csv(filename)
   counts <- counts[-1,]
   counts_yr_chunks <- split(counts,
                             ceiling(seq_along(counts$tick)/365))
   
-  full_result <- inc_func(counts, counts_yr_chunks, c("infected_via_transmission"), "uninfected")
-  a_result <- inc_func(counts, counts_yr_chunks, c("infected_via_transmission_A"), "uninfected_A")
-  b_result <- inc_func(counts, counts_yr_chunks, c("infected_via_transmission_B"), "uninfected_B")
+  full_result <- inc_func(counts_yr_chunks, c("infected_via_transmission"), c("uninfected"))
+  trans_range_result <- sapply(seq(1, length(ranges) - 1, by=2), function(x) {
+    numerators <- sprintf("infected_via_transmission_%s", seq(ranges[x], ranges[x+1]))
+    denominator <- sprintf("uninfected_%s", seq(ranges[x], ranges[x+1]))
+    inc_func(counts_yr_chunks, numerators, denominator)
+  })
   
-  ex_full_result <- inc_func(counts, counts_yr_chunks, c("infected_via_transmission", "infected_externally"), "uninfected")
-  ex_A_result <- inc_func(counts, counts_yr_chunks, c("infected_via_transmission_A", "infected_external_A"), "uninfected_A")
-  ex_B_result <- inc_func(counts, counts_yr_chunks, c("infected_via_transmission_B", "infected_external_B"), 
-                              "uninfected_B")
+  ex_full_result <- inc_func(counts_yr_chunks, c("infected_via_transmission", "infected_externally"), c("uninfected"))
+  ex_trans_range_result <- sapply(seq(1, length(ranges) - 1, by=2), function(x) {
+    # unlist(c(sprintf("vertex_count_%s", seq(1:4)), sprintf("foo_%s", seq(1:4))))
+    numerators <- unlist(c(sprintf("infected_via_transmission_%s", seq(ranges[x], ranges[x+1])), 
+                           sprintf("infected_external_%s",  seq(ranges[x], ranges[x+1]))))
+    denominator <- sprintf("uninfected_%s", seq(ranges[x], ranges[x+1]))
+    inc_func(counts_yr_chunks, numerators, denominator)
+  })
   
-  ee_full_result <- inc_func(counts, counts_yr_chunks, c("infected_via_transmission", "infected_externally", 
-                                                         "infected_at_entry"), "uninfected")
-  ee_A_result <- inc_func(counts, counts_yr_chunks, c("infected_via_transmission_A", "infected_external_A", 
-                                                        "infected_at_entry_A"), "uninfected_A")
-  ee_B_result <- inc_func(counts, counts_yr_chunks, c("infected_via_transmission_B", "infected_external_B", 
-                                                          "infected_at_entry_B"), "uninfected_B")
-
-
-  newinf_full_result <- calc_new_inf(counts, counts_yr_chunks, c("infected_via_transmission"))
-  newinf_a_result <- calc_new_inf(counts, counts_yr_chunks, c("infected_via_transmission_A"))
-  newinf_b_result <- calc_new_inf(counts, counts_yr_chunks, c("infected_via_transmission_B"))
-
-  newinf_ex_full_result <- calc_new_inf(counts, counts_yr_chunks, c("infected_via_transmission", "infected_externally"))
-  newinf_ex_a_result <- calc_new_inf(counts, counts_yr_chunks, c("infected_via_transmission_A", "infected_external_A"))
-  newinf_ex_b_result <- calc_new_inf(counts, counts_yr_chunks, c("infected_via_transmission_B", "infected_external_B"))
-
-  newinf_ee_full_result <- calc_new_inf(counts, counts_yr_chunks, c("infected_via_transmission", "infected_externally", "infected_at_entry"))
-  newinf_ee_a_result <- calc_new_inf(counts, counts_yr_chunks, c("infected_via_transmission_A", "infected_external_A", "infected_at_entry_A"))
-  newinf_ee_b_result <- calc_new_inf(counts, counts_yr_chunks, c("infected_via_transmission_B", "infected_external_B", "infected_at_entry_B"))
+  ee_full_result <- inc_func(counts_yr_chunks, c("infected_via_transmission", "infected_externally", 
+                                                         "infected_at_entry"), c("uninfected"))
+  ee_range_result <- sapply(seq(1, length(ranges) - 1, by=2), function(x) {
+    # unlist(c(sprintf("vertex_count_%s", seq(1:4)), sprintf("foo_%s", seq(1:4))))
+    numerators <- unlist(c(sprintf("infected_via_transmission_%s", seq(ranges[x], ranges[x+1])), 
+                           sprintf("infected_external_%s",  seq(ranges[x], ranges[x+1])),
+                           sprintf("infected_at_entry_%s", seq(ranges[x], ranges[x+1]))))
+    denominator <- sprintf("uninfected_%s", seq(ranges[x], ranges[x+1]))
+    inc_func(counts_yr_chunks, numerators, denominator)
+  })
   
+  newinf_full_result <- calc_new_inf(counts_yr_chunks, c("infected_via_transmission"))
+  newinf_range_result <-  sapply(seq(1, length(ranges) - 1, by=2), function(x) {
+    numerators <- sprintf("infected_via_transmission_%s", seq(ranges[x], ranges[x+1]))
+    calc_new_inf(counts_yr_chunks, numerators)
+  })
   
-   return(c(full_result, a_result, b_result,
-            ex_full_result, ex_A_result, ex_B_result,
-            ee_full_result, ee_A_result, ee_B_result,
-            newinf_full_result, newinf_a_result, newinf_b_result,
-            newinf_ex_full_result, newinf_ex_a_result, newinf_ex_b_result,
-            newinf_ee_full_result, newinf_ee_a_result, newinf_ee_b_result
-            ))
+  newinf_ex_full_result <- calc_new_inf(counts_yr_chunks, c("infected_via_transmission", "infected_externally"))
+  newinf_ex_range_result <-  sapply(seq(1, length(ranges) - 1, by=2), function(x) {
+    numerators <- unlist(c(sprintf("infected_via_transmission_%s", seq(ranges[x], ranges[x+1])), 
+                           sprintf("infected_external_%s",  seq(ranges[x], ranges[x+1]))))
+    calc_new_inf(counts_yr_chunks, numerators)
+  })
+  
+  newinf_ee_full_result <- calc_new_inf(counts_yr_chunks, c("infected_via_transmission", "infected_externally", "infected_at_entry"))
+  newinf_ee_range_result <-  sapply(seq(1, length(ranges) - 1, by=2), function(x) {
+    numerators <- unlist(c(sprintf("infected_via_transmission_%s", seq(ranges[x], ranges[x+1])), 
+                           sprintf("infected_external_%s",  seq(ranges[x], ranges[x+1])),
+                           sprintf("infected_at_entry_%s", seq(ranges[x], ranges[x+1]))))
+    calc_new_inf(counts_yr_chunks, numerators)
+  })
+  
+  return (unlist(c(full_result, trans_range_result, ex_full_result, ex_trans_range_result, ee_full_result, ee_range_result,
+                   newinf_full_result, newinf_range_result, newinf_ex_full_result, newinf_ex_range_result,
+                   newinf_ee_full_result, newinf_ee_range_result)))
 }
 
-summarize_pop_size <- function(filename="counts.csv"){
+summarize_pop_size <- function(filename="counts.csv", ranges){
     counts <- read.csv(filename)
-    return(c(tail(counts$vertex_count, 1), tail(counts$vertex_count_A, 1), tail(counts$vertex_count_B, 1)))
+    
+    pop_sizes <- sapply(seq(1, length(ranges) - 1, by=2), function(x) {
+      v_counts <- sprintf("vertex_count_%s", seq(ranges[x], ranges[x+1]))
+      sum_v_counts <- rowSums(counts[v_counts])
+      tail(sum_v_counts, 1)
+    })
+    
+    return(unlist(c(tail(counts$vertex_count, 1), pop_sizes)))
 }
