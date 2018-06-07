80f5c58527112e7d2f096a8c8aaaa6cd578e1bde
Aditya Khanna <khanna7@uw.edu>
2018-06-07 08:40:09 -0500

    Update chi_params_nonderived.R
    update daily.entry.rate here since default_params.sh has been deactivated.

--- transmission_model/r/common/chi_params_nonderived.R
+++ transmission_model/r/common/chi_params_nonderived.R
@@ -37,7 +37,7 @@
    ## DEMOGRAPHIC
    min.age <- 18
    max.age <- 34
-   daily.entry.rate <- 0.75
+   daily.entry.rate <- 1.0 #rate for n=5000
    ## distribution of ages (between min and max)
    ## number of births (n.births: for now take it as 1% per year)
    ## age-specific mortality rates (ASMR), adjusted for HIV/AIDS-related deaths
