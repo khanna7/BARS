03afb97b75d1f124934f3f0cbbb6894ebabc6509
Daniel Sheeler <dsheeler@pobox.com>
2021-02-12 16:00:18 -0600

    Add substance user attributes to nw for simulate

--- transmission_model/src/Model.cpp
+++ transmission_model/src/Model.cpp
@@ -90,7 +90,10 @@ struct PersonToVALForSimulate {
 
         return List::create(Named("na") = false, Named("vertex_names") = idx, Named("role_main") = v->steady_role(),
                 Named("role_casual") = v->casual_role(), Named("inf.status") = v->isInfected(),
-                Named("diagnosed") = v->isDiagnosed(), Named("age") = v->age(), Named("sqrt.age") = sqrt(v->age()));
+                Named("diagnosed") = v->isDiagnosed(), Named("age") = v->age(), Named("sqrt.age") = sqrt(v->age()),
+                Named("crack.user") = v->isSubstanceUser(SubstanceUseType::CRACK),
+                Named("meth.user") = v->isSubstanceUser(SubstanceUseType::METH),
+                Named("ecstasy.user") = v->isSubstanceUser(SubstanceUseType::ECSTASY));
     }
 };
 
