d25ea05c0070843127f65e9eb1e8ad0e20babbc2
Aditya S Khanna <khanna7@uw.edu>
2021-11-15 15:42:54 -0600

    analysis code

---
+++ transmission_model/swift_proj/data/data-analysis/v54-calibrate-chicago.R
@@ -0,0 +1,222 @@
+rm(list=ls())
+
+options(tibble.print_max = 100, tibble.print_min = 100)
+options(tibble.width = Inf)
+
+# Libraries ----
+
+library(dplyr)
+library(ggplot2)
+
+# Data ----
+
+full_res <- read.csv("../data/v54-calibrate-chicago-final_results.csv", header=FALSE) #final results
+loc <- "../../v54-calibrate-chicago"
+
+in1 <- read.table(paste0(loc,"/instance_1/output/parameters.txt"))
+#param_list <- read.csv("../../../data/sens_param_six_swp_fine_grained_min_ch_inf_low.csv")
+upf <- read.table(paste0(loc, "/calibrate-chicago.txt"), sep=",")
+
+# Generate Response Dataframe ‚----
+
+all_responses <- full_res[,c(1, 2, 4, 6, seq(17, 97, by=10), 98:111)]
+names(all_responses) <- c("instance", 
+                          "prevalence", "prev20to30", "prevge30",
+                          "inc_transm_full", "inc_transm_20to30", "inc_transm_ge30",
+                          "inc_transm_ext_full", "inc_transm_ext_20to30", "inc_transm_ext_ge30",
+                          "inc_transm_ext_ent_full", "inc_transm_ext_ent_20to30", "inc_transm_ext_ent_ge30",
+                          "newinf_transm_full", "newinf_transm_20to30", "newinf_transm_ge30",
+                          "newinf_transm_ext_full", "newinf_transm_ext_20to30", "newinf_transm_ext_ge30",
+                          "newinf_transm_ext_ent_full", "newinf_transm_ext_ent_20to30", "newinf_transm_ext_ent_ge30",
+                          "popsize_full", "popsize_20to30", "popsize_ge30",
+                          "vl_supp_per_pos", "vl_supp_per_diag"
+)
+
+n.scen <- 540
+n.seed <- 10
+
+all_responses$param_set <- rep(1:n.scen, each=n.seed)
+all_responses$seed <- rep(1:n.seed, n.scen)
+
+param_list$param_set <- rep(1:n.scen, each=n.seed)
+param_list$seed <- rep(1:n.seed, n.scen)
+
+# Compute means by seeds ----
+by_param_set <- group_by(all_responses, param_set)
+responses <- summarise(by_param_set,
+                       prevalence = mean(prevalence),
+                       prev_20to30 = mean(prev20to30),
+                       prev_ge30 = mean(prevge30),
+                       inc_transm_full = mean(inc_transm_full), #transm. olny
+                       inc_transm_20to30 = mean(inc_transm_20to30),
+                       inc_transm_ge30 = mean(inc_transm_ge30),
+                       inc_transm_ext_full = mean(inc_transm_ext_full), #transm+ext.
+                       inc_transm_ext_20to30 = mean(inc_transm_ext_20to30), 
+                       inc_transm_ext_ge30 = mean(inc_transm_ext_ge30),
+                       inc_transm_ext_ent_full = mean(inc_transm_ext_ent_full),#transm+ext+entry
+                       inc_transm_ext_ent_20to30 = mean(inc_transm_ext_ent_20to30),
+                       inc_transm_ext_ent_ge30 = mean(inc_transm_ext_ent_ge30),
+                       newinf_transm_full = mean(newinf_transm_full), #transm. olny
+                       newinf_transm_20to30 = mean(newinf_transm_20to30),
+                       newinf_transm_ge30 = mean(newinf_transm_ge30),
+                       newinf_transm_ext_full = mean(newinf_transm_ext_full), #transm+ext.
+                       newinf_transm_ext_20to30 = mean(newinf_transm_ext_20to30), 
+                       newinf_transm_ext_ge30 = mean(newinf_transm_ext_ge30),
+                       newinf_transm_ext_ent_full = mean(newinf_transm_ext_ent_full),#transm+ext+entry
+                       newinf_transm_ext_ent_20to30 = mean(newinf_transm_ext_ent_20to30),
+                       newinf_transm_ext_ent_ge30 = mean(newinf_transm_ext_ent_ge30),
+                       popsize_full = mean(popsize_full),
+                       popsize_20to30 = mean(popsize_20to30),
+                       popsize_ge30 = mean(popsize_ge30),
+                       vl_supp_per_pos = mean(vl_supp_per_pos),
+                       vl_supp_per_diag = mean(vl_supp_per_diag)
+)
+
+dim(responses)
+
+# Reasonable Regions ----
+
+View(all_responses)
+View(responses)
+apply(responses, 2, summary)
+
+reas_inc <- intersect(which(responses$inc_transm_full >= 4),
+                      which(responses$inc_transm_full <= 8))
+
+reas_prev <- intersect(which(responses$prevalence >= 25),
+                       which(responses$prevalence <= 40))
+
+reas_region <- intersect(reas_inc, reas_prev)
+length(reas_region)
+
+res_dt <-
+  filter(responses, param_set %in% reas_region) %>% 
+  select(
+    param_set,
+    prevalence,
+    prev_20to30,
+    prev_ge30,
+    inc_transm_full,
+    inc_transm_20to30,
+    inc_transm_ge30,
+    newinf_transm_full,
+    newinf_transm_20to30,
+    newinf_transm_ge30
+  )
+
+apply(res_dt, 2, summary)
+View(res_dt)
+
+## below is needed because `param_list` contains each parameter combination
+## repeated 10 times
+unique_param_sets <- 
+  param_list %>% 
+  select(-c(X, global.random.seed, seed))%>% 
+  group_by(param_set) %>%
+  summarise_all(mean)
+
+## unique parameters that produce reasonable results
+reas_param_tbl <- 
+  unique_param_sets %>%
+  filter(row_number() %in% reas_region)
+
+## obtain instance numbers from parameter sets
+   instances_of_interest <- lapply(reas_region, 
+                                   function (x)
+                                   ((x-1)*10+1):(x*10)
+   )
+   instances_to_use <- unlist(instances_of_interest[which(
+     reas_param_tbl$param_set == 25)])
+
+## Check outcomes on instances of interest      
+   View(all_responses[instances_to_use,])
+   calib_dt <- round(all_responses[instances_to_use,], 2)
+   write.csv(calib_dt, file="out/v54-calibrate-chicago_dt.csv")
+   
+## Check parameter sets on instances to use   
+   View(param_list[instances_to_use,])
+   View(unique_param_sets[which(unique_param_sets$param_set == 25),-1])
+   View(unique_param_sets[265,-1])
+# Analyze details in networks generated from selected instance ----
+library(network)
+library(ergm)
+   
+   # read networks from specific instance data
+   main_net <- readRDS("../../v54-calibrate-chicago/instance_245/output/main_network_36500.RDS")
+   casual_net <- readRDS("../../v54-calibrate-chicago/instance_245/output/casual_network_36500.RDS")
+   parameters <- read.table("../../v54-calibrate-chicago/instance_245/output/parameters.txt")
+   
+   # check main and casual networks
+   network.size(main_net)
+   network.edgecount(main_net)
+   degreedist(main_net)/sum(degreedist(main_net))
+     
+   network.size(casual_net)
+   network.edgecount(casual_net)
+   degreedist(casual_net)/sum(degreedist(casual_net))
+   
+# Annualized incidences in instance_248 ----
+   
+
+  source("../../../R/ph_summarize_functions.R")
+  ranges = c(20, 29, 30, 34)
+   
+  dt <- summarize_directory("/project/khanna7/Projects/midway2/BARS/transmission_model/swift_proj/experiments/v54-calibrate-chicago-instance_248", ranges=ranges)
+  saveRDS(object = dt, file = "out/v54-instance_248.RDS")
+
+  prev.dt <- dt[["prevs"]] %>%
+    mutate(ycat = as.numeric(ycat))
+  inc.dt <- dt[["incs"]] %>%
+    mutate(ycat = as.numeric(ycat))
+  
+  prev.dt <- as.data.table(prev.dt)
+  inc.dt <- as.data.table(inc.dt)
+  
+  n.years <- max(prev.dt$ycat)  
+  n.seed <- 1
+  n.scen <- 1
+  
+  # obtain tenth year prevalence 
+  #prev.dt <- dt[["prevs"]]
+  prev.dt[, param_set := rep(1:n.scen, each=n.seed*n.years)]
+  prev.dt[, seed := rep(rep(1:n.seed, each=n.years), n.scen)]
+  avg.prev.across.years <- 
+    prev.dt[, .(avg_prev_across_years = mean(mean),
+                avg_prev_across_year_age1 = mean(mean_1),
+                avg_prev_across_year_age2 = mean(mean_2)),
+            by=param_set]
+  prev.dt.10 <- prev.dt[ycat == n.years]
+  avg.prev.10yr <- prev.dt.10[, .(avg = mean(mean), 
+                                  avg_1=mean(mean_1), 
+                                  avg_2=mean(mean_2)), 
+                              by=param_set]
+  
+  # obtain tenth year incidence
+  inc.dt[, param_set := rep(1:n.scen, each=n.seed*n.years)]
+  inc.dt[, seed := rep(rep(1:n.seed, each=n.years), n.scen)]
+  inc.dt.10 <- inc.dt[ycat == 10]
+  avg.inc.across.years <-
+    inc.dt[, .(avg_inc_across_years = mean(full_inc_mean),
+               avg_inc_across_year_age1 = mean(full_inc_mean_1),
+               avg_inc_across_year_age2 = mean(full_inc_mean_2)),
+           by=param_set]
+  avg.inc.10yr <- inc.dt.10[, .(avg = mean(full_inc_mean), 
+                                avg_1=mean(full_inc_mean_1), 
+                                avg_2=mean(full_inc_mean_2),
+                                se = sd(full_inc_mean)/sqrt(length(full_inc_mean))
+  ), 
+  by=param_set]
+  
+  View(avg.inc.10yr)
+  
+  # compute annualized prevalence and incidence
+  summ_prev_dt <- prev.dt %>%
+    group_by(param_set, ycat) %>%
+    summarize(avg_annual_prev = mean(mean),
+              sd_annual_prev = sd(mean)) 
+  
+  summ_inc_dt <- inc.dt %>%
+    group_by(param_set, ycat) %>%
+    summarize(avg_annual_inc = mean(full_inc_mean),
+              sd_annual_inc = sd(full_inc_mean))
+  
---
+++ transmission_model/swift_proj/data/data-analysis/v54-degree-50pct.R
@@ -0,0 +1,162 @@
+rm(list=ls())
+
+options(tibble.print_max = 100, tibble.print_min = 100)
+options(tibble.width = Inf)
+
+# Libraries ----
+
+library(dplyr)
+library(ggplot2)
+
+# Data ----
+
+full_res <- read.csv("../data/v54-50pct-degree-final_results.csv", header=FALSE) #final results
+loc <- "../../v54-50pct-degree"
+
+in1 <- read.table(paste0(loc,"/instance_1/output/parameters.txt"))
+upf <- read.table(paste0(loc, "/50pct-degree.txt"), sep=",")
+#upf_csv <- read.csv("../../../data/serodiscordant.csv")
+
+# Generate Response Dataframe ----
+
+all_responses <- full_res[,c(1, 2, 4, 6, seq(17, 97, by=10), 98:111)]
+names(all_responses) <- c("instance", 
+                          "prevalence", "prev20to30", "prevge30",
+                          "inc_transm_full", "inc_transm_20to30", "inc_transm_ge30",
+                          "inc_transm_ext_full", "inc_transm_ext_20to30", "inc_transm_ext_ge30",
+                          "inc_transm_ext_ent_full", "inc_transm_ext_ent_20to30", "inc_transm_ext_ent_ge30",
+                          "newinf_transm_full", "newinf_transm_20to30", "newinf_transm_ge30",
+                          "newinf_transm_ext_full", "newinf_transm_ext_20to30", "newinf_transm_ext_ge30",
+                          "newinf_transm_ext_ent_full", "newinf_transm_ext_ent_20to30", "newinf_transm_ext_ent_ge30",
+                          "popsize_full", "popsize_20to30", "popsize_ge30",
+                          "vl_supp_per_pos", "vl_supp_per_diag"
+)
+n.scen <- 10
+n.seed <- 30
+all_responses$param_set <- rep(1:n.scen, each=n.seed)
+all_responses$seed <- rep(1:n.seed, 1)
+
+
+# Compute means by seeds ----
+by_param_set <- group_by(all_responses, param_set)
+responses <- summarise(by_param_set,
+                       prevalence = mean(prevalence),
+                       prev_20to30 = mean(prev20to30),
+                       prev_ge30 = mean(prevge30),
+                       inc_transm_full = mean(inc_transm_full), #transm. olny
+                       inc_transm_20to30 = mean(inc_transm_20to30),
+                       inc_transm_ge30 = mean(inc_transm_ge30),
+                       inc_transm_ext_full = mean(inc_transm_ext_full), #transm+ext.
+                       inc_transm_ext_20to30 = mean(inc_transm_ext_20to30), 
+                       inc_transm_ext_ge30 = mean(inc_transm_ext_ge30),
+                       inc_transm_ext_ent_full = mean(inc_transm_ext_ent_full),#transm+ext+entry
+                       inc_transm_ext_ent_20to30 = mean(inc_transm_ext_ent_20to30),
+                       inc_transm_ext_ent_ge30 = mean(inc_transm_ext_ent_ge30),
+                       newinf_transm_full = mean(newinf_transm_full), #transm. olny
+                       newinf_transm_20to30 = mean(newinf_transm_20to30),
+                       newinf_transm_ge30 = mean(newinf_transm_ge30),
+                       newinf_transm_ext_full = mean(newinf_transm_ext_full), #transm+ext.
+                       newinf_transm_ext_20to30 = mean(newinf_transm_ext_20to30), 
+                       newinf_transm_ext_ge30 = mean(newinf_transm_ext_ge30),
+                       newinf_transm_ext_ent_full = mean(newinf_transm_ext_ent_full),#transm+ext+entry
+                       newinf_transm_ext_ent_20to30 = mean(newinf_transm_ext_ent_20to30),
+                       newinf_transm_ext_ent_ge30 = mean(newinf_transm_ext_ent_ge30),
+                       popsize_full = mean(popsize_full),
+                       popsize_20to30 = mean(popsize_20to30),
+                       popsize_ge30 = mean(popsize_ge30),
+                       vl_supp_per_pos = mean(vl_supp_per_pos),
+                       vl_supp_per_diag = mean(vl_supp_per_diag)
+)
+
+dim(responses)
+
+View(all_responses)
+View(responses)
+apply(responses, 2, summary)
+
+# Pull out degreevector results with top 10% of degree scorers targeted ----
+
+intrv.prep.topn.0.1 <- which(upf$V6 == "degree.intrv.prep.topn=0.1")
+View(upf %>% filter(row_number() %in% intrv.prep.topn.0.1))
+
+# Compute annual incidence, prevalence, and number of infections ----
+source("../../../R/ph_summarize_functions.R")
+ranges = c(20, 29, 30, 34)
+
+dt <- summarize_directory(loc, ranges=ranges)
+saveRDS(object = dt, file = "out/v54-50pct-degree.RDS")
+dt <- readRDS("out/v54-50pct-degree.RDS")
+
+intrv.prep.topn.0.1.10years <- (((min(intrv.prep.topn.0.1)-1) * 10)+1): 
+  ((max(intrv.prep.topn.0.1)) * 10)
+
+prev.dt <- dt[["prevs"]] %>%
+  mutate(ycat = as.numeric(ycat)) %>%
+  filter(row_number() %in% intrv.prep.topn.0.1.10years)
+
+
+inc.dt <- dt[["incs"]] %>%
+  mutate(ycat = as.numeric(ycat)) %>%
+  filter(row_number() %in% intrv.prep.topn.0.1.10years)
+
+prev.dt <- as.data.table(prev.dt) 
+inc.dt <- as.data.table(inc.dt)
+
+n.years <- max(prev.dt$ycat)
+n.scen <- 5 #ONLY for testing top 10%
+#prev.dt <- dt[["prevs"]]
+prev.dt[, param_set := rep(1:n.scen, each=n.seed*n.years)]
+prev.dt[, seed := rep(rep(1:n.seed, each=n.years), n.scen)]
+avg.prev.across.years <- 
+  prev.dt[, .(avg_prev_across_years = mean(mean),
+              avg_prev_across_year_age1 = mean(mean_1),
+              avg_prev_across_year_age2 = mean(mean_2)),
+          by=param_set]
+prev.dt.10 <- prev.dt[ycat == n.years]
+avg.prev.10yr <- prev.dt.10[, .(avg = mean(mean), 
+                                avg_1=mean(mean_1), 
+                                avg_2=mean(mean_2)), 
+                            by=param_set]
+
+# obtain tenth year incidence
+inc.dt[, param_set := rep(1:n.scen, each=n.seed*n.years)]
+inc.dt[, seed := rep(rep(1:n.seed, each=n.years), n.scen)]
+inc.dt.10 <- inc.dt[ycat == n.years]
+avg.inc.across.years <-
+  inc.dt[, .(avg_inc_across_years = mean(full_inc_mean),
+             avg_inc_across_year_age1 = mean(full_inc_mean_1),
+             avg_inc_across_year_age2 = mean(full_inc_mean_2)),
+         by=param_set]
+avg.inc.10yr <- inc.dt.10[, .(avg = mean(full_inc_mean), 
+                              avg_1=mean(full_inc_mean_1), 
+                              avg_2=mean(full_inc_mean_2),
+                              se = sd(full_inc_mean)/sqrt(length(full_inc_mean))), 
+                          by=param_set]
+
+View(avg.inc.10yr)
+
+# obtain annualized number of new infections
+inct.dt.by.param.ycat <- inc.dt %>% 
+  group_by(param_set, ycat)
+tot_inc_sum_dt <- (dt[["incs"]][["tot_inc_sum"]])
+
+tot.newinf <-
+  inc.dt[, .(sum_newinf_across_years = sum(tot_inc_sum),
+             sum_newinf_across_year_age1 = sum(tot_inc_sum_1),
+             sum_newinf_across_year_age2 = sum(tot_inc_sum_2)),
+         by=param_set]
+
+tot.newinf.across.years <-
+  inc.dt %>% 
+  group_by(param_set, ycat) %>%
+  summarise(sum_newinf_across_years = sum(tot_inc_sum)/n.years,
+            sum_newinf_across_year_age1 = sum(tot_inc_sum_1)/n.years,
+            sum_newinf_across_year_age2 = sum(tot_inc_sum_2)/n.years
+  )
+
+
+View(
+  tot.newinf.across.years
+)
+
+
